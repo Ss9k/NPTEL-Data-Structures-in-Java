994ed2a36c223725e2e2cedaf00cc8dbfc31f99f
SHRIYANSHU SINGH <163839101+Ss9k@users.noreply.github.com>
2025-08-05 22:15:01 +0530

    Add files via upload

---
+++ Lecture 1.2/SwapTest4.java
@@ -0,0 +1,24 @@
+class Person {
+	String name;
+	float marks;
+	Person (String name, float marks) {
+	this. name = name; 
+	this.marks = marks;
+	}
+}
+
+class SwapTest4{
+	public static void swap (Object x, Object y) {
+	Object t;
+	t = x;
+	x = y;
+	y = t;
+	}
+public static void main (String args []) {
+	Object p1 = new Person ("Sumit", 99.9f);
+	Object p2 = new Person ("Rahul", 66.6f);
+	System.out.println ("p1 = " + p1 + "y ="+p2);
+	swap(p1, p2);
+	System. out.println ("p1 = "+p1+ "y = "+p2);
+	}
+}
---
+++ Lecture 1.2/VarargsMethodDemo2.java
@@ -0,0 +1,20 @@
+//varargs method using ellipsis
+class VarargsMethodDemo2 {
+//Defining a varargs method using ellipsis
+	static void varargsMethod2 (int ... v) {
+		System.out.println ("Number of arguments: "+ v.length) ;
+		for (int i: v) // For each item i in array v
+		System.out.print (i +" ");
+		System.out.println();
+}
+
+	public static void main (String args []){
+		// Calling the varargs method with variable arguments
+		
+		varargsMethod2 (1, -2, 3, -4); // Four parameters
+
+		varargsMethod2 (9); // One parameter
+
+		varargsMethod2 (); // no parameter
+	}
+}
